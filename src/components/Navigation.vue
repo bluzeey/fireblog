78b26df261612998b95a263fb6129e255e016692
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-05 14:58:04 +0530

    Setting up fireblog template

---
+++ src/components/Navigation.vue
@@ -0,0 +1,46 @@
+<template>
+<header>
+    <nav class="container">
+        <div class="branding">
+            <router-link class="header" :to="{name:'Home'}">
+            Fireblog
+            </router-link>
+            <div class="nav">
+                <ul>
+                    <router-links class="link" to="#">Home</router-links>
+                    <router-links class="link" to="#">Blogs</router-links>
+                    <router-links class="link" to="#">Create Post</router-links>
+                    <router-links class="link" to="#">Login/Register</router-links>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <MenuIcon/>
+    <transition name="mobile-nav">
+                <ul>
+                    <router-links class="link" to="#">Home</router-links>
+                    <router-links class="link" to="#">Blogs</router-links>
+                    <router-links class="link" to="#">Create Post</router-links>
+                    <router-links class="link" to="#">Login/Register</router-links>
+                </ul>
+    </transition>
+</header>  
+</template>
+
+<script>
+import MenuIcon from '../assets/Icons/bars-regular.svg'
+export default {
+     name:'navigation',
+     components:{
+         MenuIcon
+         }
+}
+</script>
+
+<style lang="scss" scoped>
+header{
+    background-color:#fff;
+    padding:0 25px;
+    box-shadow:0 4px 6px -1px;
+}
+</style>
---
+++ src/firebase/firebaseInit.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQvVXnq1lyQdSmKOToaxpFzHssc2W-eOw",
+  authDomain: "fireblog-clone.firebaseapp.com",
+  projectId: "fireblog-clone",
+  storageBucket: "fireblog-clone.appspot.com",
+  messagingSenderId: "336313726317",
+  appId: "1:336313726317:web:cb5191174af50c7e631664"
+
+};
+
+const app = initializeApp(firebaseConfig);
+const timestamp=firebase.firestore.FieldValue.serverTimestamp;
+
+export{timestamp};
+export default firebaseApp.firestore();
