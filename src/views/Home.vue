3b8f10edc36d9b2d024a2c73c1a4d929dc88b451
Sahil Maheshwari <sahilm1711@gmail.com>
2021-10-07 23:02:54 +0530

    Update and added Footer Component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,36 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <BlogPost/>
+    <BlogPost/>
   </div>
 </template>
 
 <script>
+import BlogPost from '../components/BlogPost.vue'
 export default {
   name: "Home",
   components: {},
+  data(){
+     return {
+      welcomeScreen: {
+        title: "Welcome!",
+        blogPost: "Weekly blog articles with all things programming including HTML, CSS, JavaScript and more. Register today to never miss a post!",
+        welcomeScreen: true,
+        photo: "coding",
+      },
+      simpleBlogPost: [
+        {
+          title: "This is a Filter Title!",
+          blogHTML: "This is a Filter blog post title!",
+          blogCoverPhoto: "beautiful-stories"
+        },
+         {
+          title: "This is a Filter Title 2!",
+          blogHTML: "This is a Filter blog post title!",
+          blogCoverPhoto: "designed-for-everyone"
+        }
+      ],
+    }
+  }
 };
 </script>
